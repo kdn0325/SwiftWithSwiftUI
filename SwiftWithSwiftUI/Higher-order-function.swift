4b9474de63cfd4727abffd4be2c2267e7211e38e
kdn0325 <dn10003@gmail.com>
2025-05-06 23:05:12 +0900

    feat: Add Loop.swift and Higher-order-function.swift files with SwiftUI implementations

---
+++ SwiftWithSwiftUI/Higher-order-function.swift
@@ -0,0 +1,18 @@
+//
+//  Higher-order-function.swift
+//  SwiftWithSwiftUI
+//
+//  Created by Abel on 5/6/25.
+//
+
+import SwiftUI
+
+struct Higher_order_function: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Higher_order_function()
+}
